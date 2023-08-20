56111cf700e031fd237990fa03aa5443cff251c2
angeljmercado <mercadomilletangel@gmail.com>
2023-08-19 20:40:49 -0700

    Define method to check if hotel is available

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import pandas
 df = pandas.read_csv("hotels.csv")
 
 class Hotel:
-    def __init__(self, id):
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
         pass
+
     def book(self):
         pass
+
     def available(self):
-        pass
+        """Checks the database to see if the selected hotel is available"""
+        check_available = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if check_available == "yes":
+            return True
+        else:
+            return False
 
 class TicketReservation:
     def __init__(self, name, hotel):
@@ -17,8 +25,8 @@ class TicketReservation:
         pass
 
 print(df)
-id = input("Enter the id of the hotel: ")
-hotel = Hotel(id)
+hotel_id = input("Enter the id of the hotel: ")
+hotel = Hotel(hotel_id)
 if hotel.available():
     hotel.book()
     name = input("Enter your name: ")
