bbc10211e9daa7b3dee2e842b2e50f968062b77d
angeljmercado <mercadomilletangel@gmail.com>
2023-08-27 14:40:11 -0700

    Define class for credit card payments

--- main.py
+++ main.py
@@ -1,37 +1,52 @@
 import pandas
 
 df = pandas.read_csv("hotels.csv", dtype={"id":str})
+df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
 
 class Hotel:
-    def __init__(self, hotel_id):
-        self.hotel_id = hotel_id
-        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
+    def __init__(self, hotel_id_local):
+        self.hotel_id_local = hotel_id_local
+        self.name = df.loc[df["id"] == self.hotel_id_local, "name"].squeeze()
+        self.city = df.loc[df["id"] == self.hotel_id_local, "city"].squeeze()
 
     def book(self):
         """Books a hotel by changing the available field in the database to no"""
-        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.loc[df["id"] == self.hotel_id_local, "available"] = "no"
         df.to_csv("hotels.csv", index=False)
 
     def available(self):
         """Checks the database to see if the selected hotel is available"""
-        check_available = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        check_available = df.loc[df["id"] == self.hotel_id_local, "available"].squeeze()
         if check_available == "yes":
             return True
         else:
             return False
 
 class TicketReservation:
-    def __init__(self, name, hotel):
-        self.name = name
-        self.hotel = hotel
+    def __init__(self, name_object, hotel_object):
+        self.name_object = name_object
+        self.hotel_object = hotel_object
 
     def generate(self):
         content= f"""
         Thank you for your reservation!
         Here is the booking information:
-        Name: {self.name}
-        Hotel: {self.hotel.name}"""
+        Name: {self.name_object}
+        Hotel: {self.hotel_object.name}
+        City: {self.hotel_object.city}"""
         return content
+
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+        
+    def validate(self, exp, holder_name, cvc):
+        card_data = {"number": self.number, "expiration": exp, "holder": holder_name, "cvc": cvc}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
 
 
 print(df)
@@ -39,9 +54,13 @@ hotel_id = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_id)
 
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation_ticket = TicketReservation(name, hotel)
-    print(reservation_ticket.generate())
+    credit_card = CreditCard("1234")
+    if credit_card.validate("12/26", "JOHN SMITH", "123"):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation_ticket = TicketReservation(name, hotel)
+        print(reservation_ticket.generate())
+    else:
+        print("There was a problem with the payment method.")
 else:
     print("Hotel is not available.")
